2d8500c7a8dd8815fa0251ba474eefa3c8cbb3b9
Sergey Manzhos <sergey.manzhos@outlook.com>
2023-02-02 17:34:17 +0700

    add skeleton app

---
+++ OpenCV_app/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace OpenCV_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Go_CLick(object sender, RoutedEventArgs e)
+        {
+            ImageBox_result.Source = ImageBox_sourse.Source;
+        }
+
+        private void SaveImage(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadImage(object sender, MouseButtonEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageBox_sourse.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+    }
+}
