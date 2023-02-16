2fb2e6ee0510d77d22e95f2f3b626ed37c7add60
Sergey Manzhos <sergey.manzhos@outlook.com>
2023-02-17 00:54:57 +0700

    collapse animation work

--- OpenCV2_photoapp/FilterControl.xaml.cs
+++ OpenCV2_photoapp/FilterControl.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace OpenCV2_photoapp;
 
@@ -8,9 +11,108 @@ public partial class FilterControl : UserControl
     public FilterControl()
     {
         InitializeComponent();
-        this.DataContext = this;
-
+        DataContext = this;
     }
 
+    private static UIElement FindElementByName(DependencyObject? parent, string name)
+    {
+        switch (parent)
+        {
+            case null:
+                return null;
+            case FrameworkElement element when element.Name == name:
+                return element;
+        }
+
+        var count = VisualTreeHelper.GetChildrenCount(parent);
+        for (var i = 0; i < count; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            var result = FindElementByName(child, name);
+            if (result != null) return result;
+        }
+
+        return null;
+    }
+
+    private static T? GetParentOfType<T>(DependencyObject child) where T : DependencyObject
+    {
+        var parent = VisualTreeHelper.GetParent(child);
+        return parent switch
+        {
+            null => null,
+            T parentOfType => parentOfType,
+            _ => GetParentOfType<T>(parent)
+        };
+    }
+
+
+    private void RunAnim(int type)
+    {
+        var element = FindElementByName(GetParentOfType<Window>(this), TargetElementName);
+        if (element == null) return;
+
+        var fromSeconds = TimeSpan.FromSeconds(0.5);
+        var calcHeight = 25*Convert.ToInt32(CountRow);
+        var HideGrid = new DoubleAnimation
+        {
+            From = calcHeight,
+            To = 0,
+            Duration = new Duration(fromSeconds)
+        };  
+        var ShowGrid = new DoubleAnimation
+        {
+            From = 0,
+            To = calcHeight,
+            Duration = new Duration(fromSeconds)
+        };
+
+        var myStoryboard = new Storyboard();
+        switch (type)
+        {
+            case 0:
+                myStoryboard.Children.Add(ShowGrid);
+                Storyboard.SetTarget(ShowGrid, element);
+                Storyboard.SetTargetProperty(ShowGrid, new PropertyPath(HeightProperty));
+                break;
+            case 1:
+                myStoryboard.Children.Add(HideGrid);
+                Storyboard.SetTarget(HideGrid, element);
+                Storyboard.SetTargetProperty(HideGrid, new PropertyPath(HeightProperty));
+                break;
+        }
+        myStoryboard.Begin();
+    }
+
+
+    public static readonly DependencyProperty TargetElementNameProperty = DependencyProperty.Register(
+        nameof(TargetElementName), typeof(string), typeof(FilterControl), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty CountRowProperty = DependencyProperty.Register(
+        nameof(CountRow), typeof(string), typeof(FilterControl), new PropertyMetadata(null));
+
+    public string TargetElementName
+    {
+        get => (string)GetValue(TargetElementNameProperty);
+        set => SetValue(TargetElementNameProperty, value);
+    }
+
+
     public string Title { get; set; }
+
+    public string CountRow
+    {
+        get => (string)GetValue(CountRowProperty);
+        set => SetValue(CountRowProperty, value);
+    }
+
+    private void Switch_On(object sender, RoutedEventArgs e)
+    {
+        RunAnim(0);
+    }
+
+    private void Switch_Off(object sender, RoutedEventArgs e)
+    {
+        RunAnim(1);
+    }
 }
--- OpenCV2_photoapp/MainWindow.xaml.cs
+++ OpenCV2_photoapp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,26 +17,16 @@ using System.Windows.Shapes;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 
-namespace OpenCV2_photoapp
+namespace OpenCV2_photoapp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-
-        // private void FilterClick(object sender, RoutedEventArgs e)
-        // {
-        //     filterParam.Visibility = (bool)firstFilter.IsChecked switch
-        //     {
-        //         true => Visibility.Visible,
-        //         false => Visibility.Collapsed
-        //     };
-        // }
+        InitializeComponent();
     }
+        
 }
